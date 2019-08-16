e769685d0dbeed91d146bf7a47e8429db83c3155
neoan <sroehrl@gmail.com>
2019-08-16 16:54:49 -0400

    Adopt PSR code style

--- User.model.php
+++ User.model.php
@@ -12,14 +12,16 @@ use Neoan3\Apps\Ops;
  * Class UserModel
  * @package Neoan3\Model
  */
-class UserModel extends IndexModel {
+class UserModel extends IndexModel
+{
     /**
      * @param $id
      *
      * @return array
      * @throws DbException
      */
-    static function byId($id) {
+    static function byId($id)
+    {
         try {
             $user = Db::easy('user.*', ['id' => '$' . $id]);
         } catch (DbException $e) {
@@ -28,7 +30,8 @@ class UserModel extends IndexModel {
 
         if (!empty($user)) {
             $user = $user[0];
-            $user['email'] = parent::first(Db::easy('user_email.email user_email.confirm_date user_email.id user_email.delete_date', ['^delete_date', 'user_id' => '$' . $id]));
+            $user['email'] = parent::first(Db::easy('user_email.email user_email.confirm_date user_email.id user_email.delete_date',
+                ['^delete_date', 'user_id' => '$' . $id]));
             if ($user['image_id']) {
                 $user['image'] = ImageModel::undeletedById($user['image_id']);
             }
@@ -52,7 +55,8 @@ class UserModel extends IndexModel {
      * @return array
      * @throws DbException
      */
-    static function find($condition = []) {
+    static function find($condition = [])
+    {
         if (!isset($condition['user.delete_date'])) {
             $condition['user.delete_date'] = '';
         }
@@ -78,10 +82,12 @@ class UserModel extends IndexModel {
      * @return array
      * @throws DbException
      */
-    static function register($email, $password, $hashed = false) {
+    static function register($email, $password, $hashed = false)
+    {
         $id = Db::uuid()->uuid;
         $confirm_code = Ops::hash(28);
-        $insertPassword = $hashed ? '=' . password_hash($password, PASSWORD_DEFAULT) : Ops::encrypt($password, $password);
+        $insertPassword = $hashed ? '=' . password_hash($password, PASSWORD_DEFAULT) : Ops::encrypt($password,
+            $password);
         Db::ask('user', ['id' => '$' . $id, 'user_type' => 'user']);
         Db::ask('user_email', ['user_id' => '$' . $id, 'email' => $email, 'confirm_code' => $confirm_code]);
         Db::ask('user_password', ['user_id' => '$' . $id, 'password' => $insertPassword]);
